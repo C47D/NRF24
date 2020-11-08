fa464946d02d5416599efda38a0adcf901ea7613
C47D <carlos.santiago.diaz@gmail.com>
2020-11-07 21:22:51 -0600

    Tests: Implement all the remaining mocks and update where needed.

--- tests/user_callbacks.c
+++ tests/user_callbacks.c
@@ -7,18 +7,27 @@
 
 void mock_ce_write(nrf_gpio state)
 {
-    mock_c()->actualCall("mock_ce_write")
-            ->withIntParameters("state", state);
+    mock_c()->actualCall(__func__)
+            ->withUnsignedIntParameters("state", state);
 }
 
 void mock_spi_xfer(const uint8_t *in, uint8_t *out, size_t xfer_size)
 {
-    (void) in;
-    (void) out;
-    (void) xfer_size;
+    mock_c()->actualCall(__func__)
+            ->withMemoryBufferParameter("in", in, xfer_size)
+            ->withMemoryBufferParameter("out", out, xfer_size)
+            ->withUnsignedIntParameters("xfer_size", (unsigned int) xfer_size);
 }
 
 void mock_delay_cb(uint32_t ms)
 {
-    (void) ms;
+    mock_c()->actualCall(__func__)
+            ->withUnsignedIntParameters("ms", ms);
+}
+
+nrf_gpio mock_irq_read(void)
+{
+    mock_c()->actualCall(__func__);
+
+    return (nrf_gpio) mock_c()->returnValue().value.unsignedIntValue;
 }
