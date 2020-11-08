f2b2b9c936a264bee5db577fd5b1e0438d00c95f
C47D <carlos.santiago.diaz@gmail.com>
2020-11-07 21:22:02 -0600

    Tests: Add mock_irq_read

--- tests/user_callbacks.h
+++ tests/user_callbacks.h
@@ -10,6 +10,7 @@ extern "C" {
 void mock_ce_write(nrf_gpio state);
 void mock_spi_xfer(const uint8_t *in, uint8_t *out, size_t xfer_size);
 void mock_delay_cb(uint32_t ms);
+nrf_gpio mock_irq_read(void);
 
 #ifdef __cplusplus
 } /* extern "C" */
