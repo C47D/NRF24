ea5bb1847a3e361020c52ef072d48b5682003d33
C47D <carlos.santiago.diaz@gmail.com>
2020-11-14 20:45:14 -0600

    Tests: Add readIrqSignal and detectAndClearIrqFlags tests.

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,6 +3,8 @@
 
 extern "C"
 {
+#include <stdio.h>
+
 /* Add mock_c */
 #include "CppUTestExt/MockSupport_c.h"
 
@@ -94,4 +96,72 @@ TEST(NRF24, readIrqSignal)
     
     mock().checkExpectations();
     mock().clear();
+
+    uint8_t retval = 0x0F;
+    const uint8_t param = NRF_CMD_NOP;
+    mock().expectOneCall("mock_spi_xfer")
+            .withMemoryBufferParameter("in", &param, 1)
+            .withOutputParameterReturning("out", &retval, 1)
+            .withParameter("xfer_size", 1);
+    
+    uint8_t status = NRF24_get_status(&radio);
+    printf("Status: 0x%02X", status);
+
+    mock().checkExpectations();
+    mock().clear();
+}
+
+TEST(NRF24, detectAndClearIrqFlags)
+{
+    int success = NRF24_init(&radio, mock_spi_xfer,
+        mock_ce_write,
+        mock_irq_read,
+        mock_delay_cb);
+    
+    (void) success;
+    
+    uint8_t retval = 0x0F;
+    const uint8_t param = NRF_CMD_NOP;
+    mock().expectOneCall("mock_spi_xfer")
+            .withMemoryBufferParameter("in", &param, 1)
+            .withOutputParameterReturning("out", &retval, 1)
+            .withParameter("xfer_size", 1);
+    
+    uint8_t status = NRF24_get_status(&radio);
+    (void) status;
+
+    mock().checkExpectations();
+    mock().clear();
+
+    uint8_t irq_shifted = (1U << NRF_RX_DR_IRQ);
+    
+    uint8_t irq_retval = irq_shifted | 0x0E;
+    mock().expectOneCall("mock_spi_xfer")
+            .withMemoryBufferParameter("in", &param, 1)
+            .withOutputParameterReturning("out", &irq_retval, 1)
+            .withParameter("xfer_size", 1);
+    
+    nrf_irq flag = NRF24_get_irq_flag(&radio);
+    printf("Flag: %x\r\n", (int) flag);
+
+    mock().checkExpectations();
+    mock().clear();
+    
+    // NRF_MAX_RT_IRQ  = 4, // 0x10
+    // NRF_TX_DS_IRQ   = 5, // 0x20
+    // NRF_RX_DR_IRQ   = 6, // 0x40
+    
+    if ((1 << NRF_MAX_RT_IRQ) & flag) {
+        printf("NRF_MAX_RT_IRQ\r\n");
+    }
+
+    if ((1 << NRF_TX_DS_IRQ) & flag) {
+        printf("NRF_TX_DS_IRQ\r\n");
+    }
+
+    if ((1 << NRF_RX_DR_IRQ) & flag) {
+        printf("NRF_RX_DR_IRQ\r\n");
+    }
+
+    CHECK_EQUAL(irq_shifted, flag);
 }
